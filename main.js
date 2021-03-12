46c0cbe75ac1e383c4b526d3928c692acefaa5a8
Oliver <rift95@live.se>
2021-03-12 18:00:35 +0100

    linting + spellcheck + future proof "clear()"

--- main.js
+++ main.js
@@ -11,15 +11,15 @@ document.onresize = () => window.location.reload();
 
 // World state
 const world = {
-	origin: [height/2, width/2],
-  transform: [0, 0],
+  origin: [height / 2, width / 2],
+  translate: [0, 0],
   dirty: true,
   tileSize: 30,
 }
 
 // Mouse state
 const mouse = {
-	prev: {
+  prev: {
     x: 0,
     y: 0,
   },
@@ -28,7 +28,7 @@ const mouse = {
 
 // Game state
 const game = {
-	aliveTiles: new Map(),
+  aliveTiles: new Map(),
 }
 
 // Mouse logic
@@ -43,11 +43,11 @@ const stopDrag = () => {
   canvas.classList.remove('drag');
 }
 const handleDrag = (ev) => {
-	if (!mouse.down) return;
-  world.transform[0] += ev.clientX - mouse.prev.x;
-  world.transform[1] += ev.clientY - mouse.prev.y;
+  if (!mouse.down) return;
+  world.translate[0] += ev.clientX - mouse.prev.x;
+  world.translate[1] += ev.clientY - mouse.prev.y;
   world.dirty = true;
-  
+
   mouse.prev.x = ev.clientX;
   mouse.prev.y = ev.clientY;
 }
@@ -56,16 +56,16 @@ document.body.addEventListener('mouseup', stopDrag);
 document.body.addEventListener('mouseleave', stopDrag);
 document.body.addEventListener('mousemove', handleDrag);
 document.body.addEventListener('touchstart', (ev) =>
-	startDrag(ev.touches[0])
+  startDrag(ev.touches[0])
 );
-document.body.addEventListener('touchend', (ev) => 
-	stopDrag(ev.touches[0])
+document.body.addEventListener('touchend', (ev) =>
+  stopDrag(ev.touches[0])
 );
-document.body.addEventListener('touchcancel', (ev) => 
-	stopDrag(ev.touches[0])
+document.body.addEventListener('touchcancel', (ev) =>
+  stopDrag(ev.touches[0])
 );
-document.body.addEventListener('touchmove', (ev) => 
-	handleDrag(ev.touches[0])
+document.body.addEventListener('touchmove', (ev) =>
+  handleDrag(ev.touches[0])
 );
 
 
@@ -73,100 +73,103 @@ document.body.addEventListener('touchmove', (ev) =>
 const ctx = canvas.getContext('2d');
 
 const clear = () => {
-	ctx.fillStyle = '#171717';
-	ctx.fillRect(0, 0, width, height);
+  ctx.save();
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  // Will always clear the right space
+  ctx.fillStyle = '#171717';
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.restore();
 }
 
-const box = (x, y, size = world.tileSize)  => {
+const box = (x, y, size = world.tileSize) => {
 
-	const centerOffset = size / 2;
-	const worldX = Math.floor(
-  	world.origin[0] + 
-    world.transform[0] + 
-    (x*size) - centerOffset
+  const centerOffset = size / 2;
+  const worldX = Math.floor(
+    world.origin[0] +
+    world.translate[0] +
+    (x * size) - centerOffset
   );
-    
-	const worldY = Math.floor(
-  	world.origin[1] + 
-    world.transform[1] + 
-    (y*size) - centerOffset
+
+  const worldY = Math.floor(
+    world.origin[1] +
+    world.translate[1] +
+    (y * size) - centerOffset
   );
-    
-	ctx.fillStyle = 'white';
-	ctx.fillRect(worldX, worldY, size, size);
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(worldX, worldY, size, size);
 }
 
 const render = () => {
-	// Queue up next animation frame
+  // Queue up next animation frame
   requestAnimationFrame(render);
-  
+
   // Check if world needs to be redrawn
-	if (!world.dirty) return;
+  if (!world.dirty) return;
   world.dirty = false;
-  
-  // Redraw world
+
   clear();
-  for (let {x, y} of game.aliveTiles.values()) {
-  	box(x, y);
+  for (let { x, y } of game.aliveTiles.values()) {
+    box(x, y);
   }
 }
 
 render();
 
-const getNeighbourCount = (tileMap, x,y) => {
-	return [
-  	tileMap.has(`${x-1},${y-1}`),
-  	tileMap.has(`${x},${y-1}`),
-  	tileMap.has(`${x+1},${y-1}`),
-  	tileMap.has(`${x-1},${y}`),
-  	tileMap.has(`${x+1},${y}`),
-  	tileMap.has(`${x-1},${y+1}`),
-  	tileMap.has(`${x},${y+1}`),
-  	tileMap.has(`${x+1},${y+1}`),
-  ].reduce((a,b) => a+Number(b), 0);
+const getNeighborCount = (tileMap, x, y) => {
+  return [
+    tileMap.has(`${x - 1},${y - 1}`),
+    tileMap.has(`${x},${y - 1}`),
+    tileMap.has(`${x + 1},${y - 1}`),
+    tileMap.has(`${x - 1},${y}`),
+    tileMap.has(`${x + 1},${y}`),
+    tileMap.has(`${x - 1},${y + 1}`),
+    tileMap.has(`${x},${y + 1}`),
+    tileMap.has(`${x + 1},${y + 1}`),
+  ].reduce((a, b) => a + Number(b), 0);
 }
 
 /*
 Conway's Game of Life
-1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-2. Any live cell with two or three live neighbours lives on to the next generation.
-3. Any live cell with more than three live neighbours dies, as if by overpopulation.
-4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+1. Any live cell with fewer than two live neighbors dies, as if by under-population.
+2. Any live cell with two or three live neighbors lives on to the next generation.
+3. Any live cell with more than three live neighbors dies, as if by overpopulation.
+4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 */
 const update = () => {
-	const tiles = new Map(game.aliveTiles);
-	for (let {x, y} of tiles.values()) {
-  	const neigbours = getNeighbourCount(tiles, x, y);
-    
+  const tiles = new Map(game.aliveTiles);
+  for (let { x, y } of tiles.values()) {
+    const neighbors = getNeighborCount(tiles, x, y);
+
     // Should cell survive
-    switch (neigbours) {
+    switch (neighbors) {
       case 2:
       case 3:
-      	// Cell survies
-      	break;
-      default:   
-      	// Cell dies
-      	game.aliveTiles.delete(`${x},${y}`);
-      	break;
+        // Cell survives
+        break;
+      default:
+        // Cell dies
+        game.aliveTiles.delete(`${x},${y}`);
+        break;
     }
-    
+
     // Should cell give birth to neighbors
     [
-    	{ x: x-1, y: y-1},
-    	{ x: x, y: y-1},
-    	{ x: x+1, y: y-1},
-    	{ x: x-1, y: y},
-    	{ x: x+1, y: y},
-    	{ x: x-1, y: y+1},
-    	{ x: x, y: y+1},
-    	{ x: x+1, y: y+1}, 
-    ].forEach(({x, y}) => {
-    	const neighbours = getNeighbourCount(tiles, x, y);
-      if (neighbours === 3) {
-      	game.aliveTiles.set(`${x},${y}`, {x,y});
+      { x: x - 1, y: y - 1 },
+      { x: x, y: y - 1 },
+      { x: x + 1, y: y - 1 },
+      { x: x - 1, y: y },
+      { x: x + 1, y: y },
+      { x: x - 1, y: y + 1 },
+      { x: x, y: y + 1 },
+      { x: x + 1, y: y + 1 },
+    ].forEach(({ x, y }) => {
+      const neighbors = getNeighborCount(tiles, x, y);
+      if (neighbors === 3) {
+        game.aliveTiles.set(`${x},${y}`, { x, y });
       }
     });
-    
+
     world.dirty = true;
   }
 }
@@ -174,66 +177,66 @@ const update = () => {
 setInterval(update, 500 /* 0.5 second */);
 
 const spawnShape = (origin, shapeArr, setDirty = true) => {
-	const offsetX = Math.floor(shapeArr[0].length/2);
-	const offsetY = Math.floor(shapeArr.length/2);
-	for (let dy = 0; dy < shapeArr.length; dy++) {
-  	for (let dx = 0; dx < shapeArr[dy].length; dx++) {
-    	const x = dx + origin[0] - offsetX;
-    	const y = dy + origin[1] - offsetY;
+  const offsetX = Math.floor(shapeArr[0].length / 2);
+  const offsetY = Math.floor(shapeArr.length / 2);
+  for (let dy = 0; dy < shapeArr.length; dy++) {
+    for (let dx = 0; dx < shapeArr[dy].length; dx++) {
+      const x = dx + origin[0] - offsetX;
+      const y = dy + origin[1] - offsetY;
       if (shapeArr[dy][dx] === '#') {
-				game.aliveTiles.set(`${x},${y}`, {x,y});
+        game.aliveTiles.set(`${x},${y}`, { x, y });
       }
     }
   }
   if (setDirty) {
-  	world.dirty = true;
+    world.dirty = true;
   }
 }
 
 const stableShapes = {
-	'block': [
-  	'##',
+  'block': [
+    '##',
     '##',
   ],
   'beehive': [
-  	'-##-',
+    '-##-',
     '#--#',
     '-##-',
   ],
   'loaf': [
-  	'-##-',
+    '-##-',
     '#--#',
     '-#-#',
     '--#-',
   ],
   'boat': [
-  	'##-',
+    '##-',
     '#-#',
     '-#-',
   ],
   'tub': [
-  	'-#-',
+    '-#-',
     '#-#',
     '-#-',
   ]
 }
 
-const oscilatingShapes = {
-	'blinker': [
-  	'###',
+const oscillatingShapes = {
+  'blinker': [
+    '###',
   ],
   'toad': [
-  	'-###',
+    '-###',
     '###-',
   ],
   'beacon': [
-  	'##--',
+    '##--',
     '#---',
     '---#',
     '--##'
   ],
   'pulsar': [
-  	'--###---###--',
+    '--###---###--',
     '-------------',
     '#----#-#----#',
     '#----#-#----#',
@@ -248,7 +251,7 @@ const oscilatingShapes = {
     '--###---###--',
   ],
   'pentadecathlon': [
-  	'###',
+    '###',
     '#-#',
     '###',
     '###',
@@ -260,15 +263,15 @@ const oscilatingShapes = {
 }
 
 const shipShapes = {
-	'glider': [
-  	'--#',
+  'glider': [
+    '--#',
     '#-#',
     '-##',
   ]
 }
 
-spawnShape([-8,0], oscilatingShapes.pentadecathlon, false);
-spawnShape([8,0], oscilatingShapes.pulsar, false);
-spawnShape([-5,-10], shipShapes.glider, false);
+spawnShape([-8, 0], oscillatingShapes.pentadecathlon, false);
+spawnShape([8, 0], oscillatingShapes.pulsar, false);
+spawnShape([-5, -10], shipShapes.glider, false);
 
 world.dirty = true;
